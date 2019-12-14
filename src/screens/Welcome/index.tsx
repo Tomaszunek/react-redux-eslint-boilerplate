00ed2b38736165707ea5c8e7968dee4651e7cbe4
Tomaszunek <pawlowskitomaszu@gmail.com>
2019-12-14 20:08:12 +0100

    add datatable

--- src/screens/Welcome/index.tsx
+++ src/screens/Welcome/index.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent, Fragment } from 'react';
 import { connect } from "react-redux";
+import ReactDataGrid from 'react-data-grid';
+
 import { IRootState } from "../../store";
 import { INCRE, DECRE } from "../../store/Counter/actions";
 
@@ -20,7 +22,14 @@ interface WelcomeProps {
 const Welcome: FunctionComponent<WelcomeProps> = (props) => {
   const { DECRE, INCRE, counter, thunkSendMessage} = props;
   const { count } = counter;
-  console.log(count)
+
+  const columns = [
+    { key: 'id', name: 'ID' },
+    { key: 'title', name: 'Title' },
+    { key: 'count', name: 'Count' } ];
+  
+  const rows = [{id: 0, title: 'row1', count: 20}, {id: 1, title: 'row1', count: 40}, {id: 2, title: 'row1', count: 60}];
+  
   return (
   <Fragment>
     <Header />
@@ -31,6 +40,11 @@ const Welcome: FunctionComponent<WelcomeProps> = (props) => {
       <span onClick={() => {DECRE({count: 2})}}>+</span>
       <span onClick={() => {INCRE({count: 2})}}>-</span>
       <span onClick={() => {thunkSendMessage({count: 2})}}>async</span>
+      <ReactDataGrid
+      columns={columns}
+      rowGetter={i => rows[i]}
+      rowsCount={3}
+      minHeight={150} />
     </section>
   </Fragment>
   )
